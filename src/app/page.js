b10284536ea3bb9ba7f768624c35b42c1931a149
@amal7784404 <amal@trackerhero.com>
2025-07-14 14:34:57 +0800

    Update Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
-import Home from "./Home";
+"use client";
 
-function Home() {
-  return <></>;
+import React from "react";
+import Products from "../component/Products";
+
+export default function HomePage() {
+  return (
+    <div className="hero">
+      <div className="card text-black bg-dark border-0 ">
+        <img src="/assets/bg.jpg" className="card-img" alt="Background  " />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            {" "}
+            <h5 className="card-title display-4 fw-bold mb-0">
+              UPCOMING SEASON COLLECTION
+            </h5>
+            <p className="card-text lead fs-2 mb-0">
+              CHECK OUT OUR NEW ARRIVALS
+            </p>
+          </div>
+        </div>
+      </div>
+      <Products />
+    </div>
+  );
 }
-
-export default Home;
